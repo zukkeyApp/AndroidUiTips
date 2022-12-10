3ea5aba9dd654bce679559a4be9e069996547a41
zukkeyApp <>
2022-12-10 13:31:10 +0900

    Replaced the main screen and carved out the cells so that they can be reused.

---
+++ app/src/main/java/com/zukkeyapp/androiduitips/ui/main/MainCellComposable.kt
@@ -0,0 +1,47 @@
+package com.zukkeyapp.androiduitips.ui.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainCellComposable(
+    number: Int,
+    title: String
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = number.toString(),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.secondary,
+            modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(name = "メイン画面のセルの表示サンプル", showBackground = true)
+@Composable
+fun MainCellComposablePreview() {
+    MainCellComposable(
+        number = 0,
+        title = "Sample"
+    )
+}
--- app/src/main/java/com/zukkeyapp/androiduitips/ui/main/MainScreen.kt
+++ app/src/main/java/com/zukkeyapp/androiduitips/ui/main/MainScreen.kt
@@ -6,10 +6,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     val list = viewModel.uiState.cellDataList
+    MainContentsComposable(list = list)
+}
+
+@Composable
+private fun MainContentsComposable(list: List<MainUiState.MainCellData>) {
     if (list.isEmpty()) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -22,15 +28,23 @@ fun MainScreen(viewModel: MainViewModel) {
             modifier = Modifier.fillMaxSize(),
         ) {
             list.map {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = it.id.toString())
-                    Text(text = it.name)
-                }
+                MainCellComposable(number = it.id, title = it.name)
             }
         }
+    }
+}
+
+@Preview(name = "スクリーン画面の一覧", showBackground = true)
+@Composable
+private fun MainScreenComposablePreview() {
+    MainContentsComposable(list = createMainCellDataList())
+}
+
+private fun createMainCellDataList(): List<MainUiState.MainCellData> {
+    return (0..10).map {
+        MainUiState.MainCellData(
+            id = it,
+            name = "Sample$it"
+        )
     }
 }
